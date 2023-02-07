8a90b9d5b04399d28d165b41c1a8eb164b281f34
Tiavina Malalaniaina <tiavinaramia@gmail.com>
2023-02-07 11:50:35 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://album.rf.gd/aHeberger/';
-$config['base_url'] = 'http://localhost/L2/S3/examples/aHeberger/';
+$config['base_url'] = 'http://localhost/takalo/takalo-takalo';
 
 /*
 |--------------------------------------------------------------------------
