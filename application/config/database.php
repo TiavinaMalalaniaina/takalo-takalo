0dd0a9d6eab3c85ab8d72b626ec74c62b3602c5b
Tsiky Aro <119358772+TsikyAro@users.noreply.github.com>
2023-05-01 11:31:37 +0300

    test

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '185.161.10.160',
 	'username' => 'codeforce',
 	'password' => 'CI3Angular!',
 	'database' => 'codeforce_takalo',
